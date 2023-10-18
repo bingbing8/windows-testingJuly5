3acd566aea7e0df1b7024e834723cce8ebc3172e
Yanbing Wange <yawang@microsoft.com>
2023-10-17 17:10:19 -0700

    uncomment

--- capz/run-k8s-e2e-test-in-pod.sh
+++ capz/run-k8s-e2e-test-in-pod.sh
@@ -14,9 +14,9 @@ log() {
 export SKIP_TEST="${SKIP_TEST:-"false"}"
 if [[ ! "$SKIP_TEST" == "true" ]]; then
     log "delete test-pod if exist"
-    #kubectl delete pod test-pod --ignore-not-found=true
-    #kubectl apply -f "$SCRIPT_ROOT"/runtestfrommgmtcluster/run-e2e-test-sa.yaml
-    #< "$SCRIPT_ROOT"/runtestfrommgmtcluster/e2etest-pod.yaml envsubst | kubectl apply -f -
+    kubectl delete pod test-pod --ignore-not-found=true
+    kubectl apply -f "$SCRIPT_ROOT"/runtestfrommgmtcluster/run-e2e-test-sa.yaml
+    < "$SCRIPT_ROOT"/runtestfrommgmtcluster/e2etest-pod.yaml envsubst | kubectl apply -f -
     max_item=50
     counter=0
     log "wait test to completed or error ..."
