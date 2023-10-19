fd01e091a8566dabb4df7181bbe38689a621ebfa
Yanbing Wange <yawang@microsoft.com>
2023-10-18 22:05:48 -0700

    latest kubectl version doesn't have match checksum

--- capz/run-k8s-e2e-test-in-pod.sh
+++ capz/run-k8s-e2e-test-in-pod.sh
@@ -22,21 +22,20 @@ log "delete test-pod if exist"
 kubectl delete pod test-pod --ignore-not-found=true
 kubectl apply -f "${SCRIPT_ROOT}"/runtestfrommgmtcluster/run-e2e-test-sa.yaml
 < "${SCRIPT_ROOT}"/runtestfrommgmtcluster/e2etest-pod.yaml envsubst | kubectl apply -f -
-max_item=50
+max_item=80
 counter=0
 log "wait tasks on pod to completed or error ..."
 ret=1
 while [ $ret -ne 0 ] && [ "$counter" -lt "$max_item" ]; do
     log "Check status again #$counter"
-    counter=$((counter + 1))        
-    #(( counter++ ))
+    counter=$((counter + 1))
     current_status=$(kubectl get pod test-pod --no-headers -o=custom-columns=:.status.phase)
     if [[ "${current_status,,}" == "failed" ]] || [[ "${current_status,,}" == "succeeded" ]]; then
         log "error occure in test-pod, exiting ..."
         counter=$max_item    
     fi
     ret=0
-    kubectl wait --timeout 3m --for=condition=Ready pod/test-pod || ret=$?
+    kubectl wait --timeout 2m --for=condition=Ready pod/test-pod || ret=$?
 done
 if [ $ret == 0 ]; then
     echo "Tests are completed. Copying artifacts from test-pod:_artifacts to ${ARTIFACTS}"
